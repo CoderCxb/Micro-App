23f2d214d0f7925544de58329e9ecf29e26e21eb
chen xiaobin <873587091@qq.com>
2022-09-30 01:45:36 +0800

    feat: 补充README

--- react-app/craco.config.js
+++ react-app/craco.config.js
@@ -9,31 +9,12 @@ module.exports =  {
       webpackConfig.output.library = `${name}-[name]`;
       webpackConfig.output.libraryTarget = 'umd';
       webpackConfig.output.chunkLoadingGlobal = `webpackJsonp_${name}`;
-      // webpackConfig.output.library = `${name}-[name]`;
-      // webpackConfig.output.libraryTarget = 'umd';
       // webpackConfig.output.jsonpFunction = `webpackJsonp_${name}`;
       return webpackConfig;
     }
   },
-  // webpack: {
-  //   config: {
-  //     output: {
-  //       library: `${name}-[name]`,
-  //       libraryTarget: 'umd',
-  //       jsonpFunction: `webpackJsonp_${name}`,
-  //     }
-  //   }
-  // },
-  // webpack: function override(config, env) {
-  //   console.log('ssSSSsssssssSSSsssssssSSSsssssssSSSsssss', config);
-  //   config.output.library = `${name}-[name]`;
-  //   config.output.libraryTarget = 'umd';
-  //   config.output.jsonpFunction = `webpackJsonp_${name}`;
-  //   return config;
-  // },
+
   devServer: (config) => {
-      config.open = false;
-      config.hot = false;
       config.headers = {
         'Access-Control-Allow-Origin': '*',
       };
